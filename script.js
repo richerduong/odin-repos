2ff0202e3cc2ce3a5d82f5cc610d56e5d25770fb
Richer Duong <duongricher@gmail.com>
2023-08-14 15:30:21 -0500

    Complete JavaScript exercise using DOM methods

--- script.js
+++ script.js
@@ -5,3 +5,32 @@ content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
 container.appendChild(content);
+
+// Create the <p> element
+const redParagraph = document.createElement('p');
+redParagraph.textContent = 'Hey I\'m red!';
+redParagraph.style.color = 'red';
+container.appendChild(redParagraph);
+
+// Create the <h3> element
+const blueHeader = document.createElement('h3');
+blueHeader.textContent = "I'm a blue h3!";
+blueHeader.style.color = 'blue';
+container.appendChild(blueHeader);
+
+// Create the <div> element
+const divElement = document.createElement('div');
+divElement.style.border = '1px solid black';
+divElement.style.backgroundColor = 'pink';
+
+// Create the <h1> element
+const headerElement = document.createElement('h1');
+headerElement.textContent = "I'm in a div";
+divElement.appendChild(headerElement);
+
+// Create the inner <p> element
+const innerParagraph = document.createElement('p');
+innerParagraph.textContent = "ME TOO!";
+divElement.appendChild(innerParagraph);
+
+container.appendChild(divElement);
